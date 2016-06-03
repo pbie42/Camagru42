5925f7f75e18a33b5d3cfda7a59b4bdd9ce4d2f9
Paul BIE <pbie@e1r9p5.42.fr>
2016-06-03 15:21:27 +0200

    Finished comment system now I need to test it and then convert it into post comments

--- php_parsers/status_system.php
+++ php_parsers/status_system.php
@@ -21,10 +21,61 @@ if (isset($_POST['action']) && $_POST['action'] == "status_post") {
   //Then we sanatize the $_POST variables that we will be given
   $type = preg_replace('#[^a-z]#', '', $_POST['type']);
   $account_name = preg_replace('#[^a-z0-9]#i', '', $_POST['user']);
+  //As with the status_reply section below we are using htmlentites to prevent malicious code.
   $data = htmlentities($_POST['data']);
   $data = mysqli_real_escape_string($db_conx, $data);
   //Now we make sure the account name exists like we did in the status_reply section
-
+  $sql = "SELECT COUNT(id) FROM users WHERE username='$account_name' AND activated='1' LIMIT 1";
+  $query = mysqli_query($db_conx, $sql);
+  $row = mysqli_fetch_row($query);
+  if ($row[0] < 1) {
+    mysqli_close($db_conx);
+    echo "account_no_exit";
+    exit();
+  }
+  //We then insert the status post into the database
+  $sql = "INSERT INTO status(account_name, author, type, data, postdate) VALUES('$account_name','$log_username','$type','$data',now())";
+  $query = mysqli_query($db_conx, $sql);
+  $id = mysqli_insert_id($db_conx);
+  mysqli_query($db_conx, "UPDATE status SET osid='$id' WHERE id='$id' LIMIT 1");
+  //Then we count posts of type a for the person posting and evalute the count.
+  $sql = "SELECT COUNT(id) FROM status WHERE author='$log_username' AND type='a'";
+  $query = mysqli_query($db_conx, $sql);
+  $row = mysqli_fetch_row($query);
+  //This part below is for if I want to have a system that auto flushes the oldest comments.
+  //This can also be used to auto flush type c and b as well. I will leave this part
+  //commented out because I will want to keep all of the posts. But good to look at in the future.
+  /*if ($row[0] > 9) {
+    $sql = "SELECT id FROM status WHERE author='log_username' AND type='a' ORDER BY id ASC LIMIT 1";
+    $query = mysqli_query($db_conx, $sql);
+    $row = mysqli_fetch_row($query);
+    $oldest = $row[0];
+    //This deletes all the posts associated with the original status ID
+    //similar to deleting all the posts in a long forum thread.
+    mysqli_query($db_conx, "DELETE FROM status WHERE osid='$oldest'");
+  }*/
+  //Last we are going to send notifcations to all of the friends of the post author.
+  //TODO need to decide whether I will allow this or not. Probably should for a bonus
+  //Can also use this for things other than comments such as whenever someone makes a
+  //new photo post. All I would have to do is adapt this for my photo_system parsing.
+  $friends = array();
+  $query = mysqli_query($db_conx, "SELECT user1 FROM friends WHERE user2='$log_username' AND accepted='1'");
+  while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+    array_push($friends, $row["user1"]);
+  }
+  $query = mysqli_query($db_conx, "SELECT user2 FROM friends WHERE user1='$log_username' AND accepted='1'");
+  while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+    array_push($friends, $row["user2"]);
+  }
+  for ($i=0; $i < count($friends); $i++) {
+    $froemd = $friends[$i];
+    $app = "Status Post";
+    $note = $log_username.' posted on: <br /><a href="user.php?u='.$account_name.'#status_'.$id.'">'.$account_name.'&#39;s Profile</a>';
+    mysqli_query($db_conx, "INSERT INTO notifications(username, initiator, app, note, date_time) VALUES('$friend','$log_username','$app','$note',now())");
+  }
+  mysqli_close($db_conx);
+  echo "post_ok|$id";
+  exit();
 }
 ?>
 <?php
